0d779e0c0ffe5ea013f5c1dd56a4c6694e8638e0
xianshenglu <xianshenglu@qq.com>
2022-07-23 13:58:41 +0800

    fix: fix typo

--- src/services/errorHandler.ts
+++ src/services/errorHandler.ts
@@ -1,7 +1,7 @@
 export class ErrorHandler {
   getFormattedError(error: any, extraMessage: string): Error {
     const originalErrorMessage = error?.message
-      ? error?.messag + "\n" + extraMessage
+      ? error?.message + "\n" + extraMessage
       : extraMessage;
     return new Error(originalErrorMessage);
   }
